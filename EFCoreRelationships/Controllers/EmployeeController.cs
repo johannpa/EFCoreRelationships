f681f3f4f7c59d71d478f6bce90a4ac8ddbae4c7
johannpa <johann_paque@hotmail.fr>
2024-04-04 18:48:25 +0200

    Create the controller for the employees

---
+++ EFCoreRelationships/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreRelationships.Data;
+using EFCoreRelationships.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployees()
+        {
+            var employees = await _appDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
