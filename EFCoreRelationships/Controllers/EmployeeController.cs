123cc2d54526cf9d2d764c61b632e561e833c32b
johannpa <johann_paque@hotmail.fr>
2024-04-04 19:57:16 +0200

    Finished all the methods and made some corrections

--- EFCoreRelationships/Controllers/EmployeeController.cs
+++ EFCoreRelationships/Controllers/EmployeeController.cs
@@ -17,6 +17,7 @@ namespace EFCoreRelationships.Controllers
             _appDbContext = appDbContext;
         }
 
+        //Read all employees from the database
         [HttpGet]
         public async Task<ActionResult<List<Employee>>> GetEmployees()
         {
@@ -24,8 +25,9 @@ namespace EFCoreRelationships.Controllers
             return Ok(employees);
         }
 
-        [HttpGet("{id : int}")]
-        public async Task<ActionResult<Employee>> GetEmployees(int id)
+        // Read one employee
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
         {
             var employee = await _appDbContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
             if (employee != null)
@@ -34,6 +36,58 @@ namespace EFCoreRelationships.Controllers
             }
 
             return NotFound();
+        }
+
+        //Add employee
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee newEmployee)
+        {
+            if (newEmployee != null)
+            {
+                _appDbContext.Employees.Add(newEmployee);
+                await _appDbContext.SaveChangesAsync();
+
+                var employees = await _appDbContext.Employees.ToListAsync();
+                return Ok(employees);
+            }
+            return BadRequest();
+        }
+
+        //Delete
+        [HttpDelete]
+        public async Task<ActionResult<List<Employee>>> DeleteEmployee(int id)
+        {
+            var employee = await _appDbContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
+            if (employee != null)
+            {
+                _appDbContext.Employees.Remove(employee);
+                await _appDbContext.SaveChangesAsync();
+                var employees = await _appDbContext.Employees.ToListAsync();
+                return Ok(employees);
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<Employee>> UpdateEmployee(Employee updateEmployee)
+        {
+            if(updateEmployee != null)
+            {
+                var employee = await _appDbContext.Employees.FirstOrDefaultAsync(e => e.Id == updateEmployee.Id);
+                if(employee != null)
+                {
+                    employee.Name = updateEmployee.Name;
+                    employee.Age = updateEmployee.Age;
+                    await _appDbContext.SaveChangesAsync();
+
+                    var employees = await _appDbContext.Employees.ToListAsync();
+                    return Ok(employees);
+                }
+                return NotFound();
+            }
+
+            return BadRequest();
         }
     }
 }
